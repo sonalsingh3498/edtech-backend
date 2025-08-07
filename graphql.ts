3bfa95dda74dd685c1aa765c706beaddc3d50a85
Sonal Singh <sonalsingh@Sonals-MacBook-Pro.local>
2025-08-08 00:36:36 +0530

    first commit

---
+++ graphql.ts
@@ -0,0 +1,23 @@
+import { ApolloServer } from 'apollo-server-micro';
+import { typeDefs } from './schema';
+import { resolvers } from './resolvers';
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startServer = server.start();
+
+export default async function handler(req: any, res: any) {
+  await startServer;
+  await server.createHandler({
+    path: '/api/graphql',
+  })(req, res);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
